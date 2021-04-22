ae642b1b1e2489622814b6ab3d1966eab08f0b11
gusek2 <43113792+gusek2@users.noreply.github.com>
2021-04-22 14:12:28 +0300

    Add files via upload

---
+++ dababy.py
@@ -0,0 +1,50 @@
+import discord
+from discord.ext import commands
+import os
+
+client = commands.Bot(command_prefix= 'dababy.')
+# dababy.help
+
+#words
+letsgo_words = ['lets go', 'lessgo', 'lessgoo','lesssgooo', 'letsgoo', 'lesgoo','lesssgo']
+answer_words = [ 'сосешь?','ты пидорас?','гей','пидарас','хуесос','еблан','долбаёб','влад','глеб','пидор','пидар','сосни',]
+
+@client.event
+
+async def on_ready():
+    print('LETS GOOO')
+@client.command(pass_content = True)
+
+@client.event
+
+async def letsgo(ctx) :
+    author = ctx.message.author
+
+    await ctx.send( f' {author.mention} LESSS GOOO')
+
+@client.event
+
+async def on_message(message):
+    msg = message.content.lower()
+    if msg in letsgo_words:
+        await message.channel.send('LESSS GOOOO')
+
+@client.event
+
+async def on_message(message):
+    msg = message.content.lower()
+    if msg in answer_words:
+         await message.channel.send('кто как обзывается тот так и называется')
+         await message.channel.send('https://allya.ru/wp-content/uploads/2020/02/dababy-1.jpg')
+
+@client.event
+
+async def on_message(message):
+    msg = message.content.lower()
+    if msg == 'пососи':
+        await message.channel.send('https://www.youtube.com/watch?v=Vq_9cTepBWA')
+#connect
+
+token = os.environ.get('BOT_TOKEN')
+
+client.run(token)
